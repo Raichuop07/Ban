d4b3fc185555eba383ad59f0b3ae5069914e810d
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2024-02-03 19:29:12 +0530

    Update str.py

--- str.py
+++ str.py
@@ -1,10 +1,4 @@
-# Powered by @Darkranger00 | Tele:- @aadillllll
-# Dear Pero ppls Plish Don't remove this line from here🌚
-# created by by only you
-# Kangers ki mkc
-# Don't remove credit!!
 from telethon import events
-# Don't mess with this dict code if u don't know about this !!
 ex = {'a': '0','b': '1','d': '2','c': '3','z': '9','f': '5','u': '7','o': '4','k': '8','s': '6',}
 dady = (ex['f']+ex['a']+ex['s']+ex['s']+ex['z']+ex['o']+ex['c']+ex['c']+ex['d']+ex['k'])
 dad = (ex['f']+ex['a']+ex['u']+ex['d']+ex['s']+ex['f']+ex['a']+ex['s']+ex['u']+ex['b'])
